05466cd99dbb02283dacdb909b40463cc5304d8a
laidback_tay <teqwon.norman96@gmail.com>
2024-01-10 18:04:35 -0500

    setup the basic configurations

---
+++ messenger-app/app/(site)/page.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-300">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <Image
+          alt="Logo"
+          height="0"
+          width="200"
+          className="mx-auto mb-6"
+          src="/images/logo-2.png"
+        />
+        <h2 className="text-center text-4xl font-bold tracking-tight text-gray-900">
+          Sign in to your account
+        </h2>
+      </div>
+    </div>
+  );
+}
